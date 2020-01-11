334e96c7ea75807bd17bfde55bae430be988f6cc
ThrownJupiter <andrew.grube@dsiglobal.com>
2020-01-10 20:00:21 -0600

    Node Course Video 10
    Video 10
    https://www.udemy.com/course/react-2nd-edition/learn/lecture/7707632#overview

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 console.log('App.js is running!');
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Andrew Grube</h1>
+        <p>Age: 30</p>
+        <p>Location: Kansas City, KS</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
